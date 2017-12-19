726d01fca76f5dee046cb1e14d436ed83c35d7f5
KangYi <kangyi_mm@163.com>
2017-12-19 16:42:41 +0800

    MJPhotoBrowser改进

---
+++ MJPhotoBrowser改进版/ImageCell.h
@@ -0,0 +1,13 @@
+//
+//  ImageCell.h
+//  MJPhotoBrowser改进版
+//
+//  Created by 康义 on 2017/12/19.
+//  Copyright © 2017年 康义. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageCell : UICollectionViewCell
+
+@end
---
+++ MJPhotoBrowser改进版/ImageCell.m
@@ -0,0 +1,18 @@
+//
+//  ImageCell.m
+//  MJPhotoBrowser改进版
+//
+//  Created by 康义 on 2017/12/19.
+//  Copyright © 2017年 康义. All rights reserved.
+//
+
+#import "ImageCell.h"
+
+@implementation ImageCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
